5ae43891a997fd61daf3a67570919d3bcae02036
tusquake <77339749+tusquake@users.noreply.github.com>
2022-07-17 08:35:11 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int i,mul,n;
+    cout<<"Enter the number : ";
+    cin>>n;
+    i=1;
+   do{
+      mul = 10*i;
+       i++;
+    cout<<mul<<"\n";
+    } while(i<=n);
+}
+// 1+2+3+4+5 = 15
